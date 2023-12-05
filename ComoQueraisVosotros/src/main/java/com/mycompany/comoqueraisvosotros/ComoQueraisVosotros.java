878acf5a3796f1812daf68d1b682851912afd991
polkgamer002 <132491784+polkgamer002@users.noreply.github.com>
2023-12-05 17:02:56 +0100

    Add files via upload

--- ComoQueraisVosotros/src/main/java/com/mycompany/comoqueraisvosotros/ComoQueraisVosotros.java
+++ ComoQueraisVosotros/src/main/java/com/mycompany/comoqueraisvosotros/ComoQueraisVosotros.java
@@ -10,16 +10,20 @@ import java.sql.*;
  * @author pablo
  */
 public class ComoQueraisVosotros {
-
+    //declaramos la url, el usuario, la contraseña para conectarnos a la base de datos
     static final String DB_URL = "jdbc:mysql://localhost:3306/jcvd";
     static final String USER = "dam2";
     static final String PASS = "1234";
+    //declaramos la query para que la ejecute en la base de datos
     static final String QUERY = "SELECT * FROM videojuegos";
 
     public static void main(String[] args) {
+        //abre una conexion a la base de datos con los datos introducidos enteriormente
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS); 
+                //crea un metodo statement para ejecutar el sql y la almacena en resultset
                 Statement stmt = conn.createStatement(); 
                 ResultSet rs = stmt.executeQuery(QUERY);) {
+                //imprime cada valor de cada columna mientras haya lineas que leer
             while (rs.next()) {
                 System.out.print("\n"+"ID: " + rs.getInt("id"));
                 System.out.print(" Nombre: " + rs.getString("Nombre"));
@@ -27,9 +31,11 @@ public class ComoQueraisVosotros {
                 System.out.print(" FechaLanzamiento: " + rs.getDate("FechaLanzamiento"));
                 System.out.print(" Compañía: " + rs.getString("Compañía"));
                 System.out.print(" Precio: " + rs.getFloat("Precio"));  
-            }   
-            
+            } 
+            //cierra el statement
+            stmt.close();
         }
+        //captura las excepciones sqlexception e imprime la pila de errores
         catch (SQLException e){
         e.printStackTrace();
     }
